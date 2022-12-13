04c6fc594380d031f5c0b00ed2c1199b1a0dff50
biricik-CT <71524881+biricik-CT@users.noreply.github.com>
2022-12-13 10:04:44 +0300

    hüseyin ömer
    @themanoftalent

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,15 @@
 // your name surname and age from user
+#include <stdio.h>
+
+int main(){
+	char name[35];
+	char surname[25];
+	
+	printf("Enter your name: ");
+	scanf("%s",name);
+	printf("\nEnter your surname : ");
+	scanf("%s",&surname);
+	
+	printf("%s %s",name,surname);
+return 0;
+}
