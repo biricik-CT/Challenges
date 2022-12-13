e8c61c6ff40eb905a5174e437541da357ecdda25
biricik-CT <71524881+biricik-CT@users.noreply.github.com>
2022-12-13 10:25:19 +0300

    Hüseyin Ömer Biricik
    @themanoftalent

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,18 @@
 // Online C compiler to run C program online
 #include <stdio.h>
+#include <stdio.h>
 int main(){
  int i,j,k,n; 
  printf("Enter the number of the row :");
  scanf("%d",&n);
  
  for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
+     for(j=0;j<n;i++){
+    
      
  }
-     printf("Akif")
+     printf("Akif\n");
  }
  
-    
+  return 0;  
 }
